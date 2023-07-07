6dc3759851f209a5ce14da5a980239f874a19970
Mazoda <oabumutlak@gmail.com>
2023-07-07 21:09:29 +0300

    Fix errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import * as React from 'react';
 import {ThemeProvider} from '@mui/material/styles';
 import {theme} from './Theme/Theme';
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
