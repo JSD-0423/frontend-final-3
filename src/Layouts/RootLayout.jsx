94f5814d8eeb343aa2b6bd348db6ffccdebdac30
Mazoda <oabumutlak@gmail.com>
2023-07-08 09:23:18 +0300

    Add: Header

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import {Outlet} from "react-router-dom";
+import Header from '../Components/Header/Header';
 
 export default function RootLayout() {
     return (
         <>
-            {/*<Header/>*/}
+            <Header/>
             <main>
 
                 <Outlet/>
