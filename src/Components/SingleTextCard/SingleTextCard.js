a0f5a2438a724bf3fba776f6b89fd9c1ba0af908
halasalhab <halasalhab0@gmail.com>
2023-07-08 20:25:58 +0200

    handpicked collections ready

---
+++ src/Components/SingleTextCard/SingleTextCard.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+
+SingleTextCard.propTypes = {
+    title: PropTypes.string.isRequired,
+    imageSrc: PropTypes.string.isRequired
+};
+
+function SingleTextCard({ title, imageSrc }) {
+
+    return (
+        <Card sx={{
+            width: "100%",
+            position: "relative",
+            borderRadius: "10px"
+        }}>
+            <CardMedia
+                sx={{
+                    height: { xs: "156px", lg: "280px" }
+                }}
+                image={imageSrc}
+                title={title}
+            />
+            <CardContent
+                sx={{
+                    backgroundColor: "transparent",
+                    position: "absolute",
+                    top: { xs: "70%", lg: "80%" },
+                    left: "-5px"
+                }}
+            >
+                <Typography gutterBottom variant="body1" component="div" sx={{
+                    fontSize: { xs: "1rem", lg: "1.5rem" },
+                    fontWeight: "fontWeightBold"
+                }}>
+                    {title}
+                </Typography>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default SingleTextCard
--- src/Theme/Theme.js
+++ src/Theme/Theme.js
@@ -41,4 +41,13 @@ export const theme = createTheme({
       main: "E626262",
     },
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
 });
