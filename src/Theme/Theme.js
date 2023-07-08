fd20e795e9b3e221024a8abd46917aeda499e924
Mazoda <oabumutlak@gmail.com>
2023-07-08 09:22:07 +0300

    Fix: Import Inter Font

--- src/Theme/Theme.js
+++ src/Theme/Theme.js
@@ -1,8 +1,12 @@
 import { createTheme } from "@mui/material/styles";
+import '@fontsource/inter/300.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/700.css';
 
 export const theme = createTheme({
   typography: {
-    fontFamily: "Inter",
+    fontFamily: "inter",
     fontWeightRegular: 400,
     fontWeightMedium: 500,
     fontWeightBold: 700,
