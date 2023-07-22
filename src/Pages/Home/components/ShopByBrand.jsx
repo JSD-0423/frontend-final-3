87ab600a8bc60eb5e258890d7a6538ba4d1452f3
Mazoda <oabumutlak@gmail.com>
2023-07-22 10:13:15 +0300

    refactor: added padding right

--- src/Pages/Home/components/ShopByBrand.jsx
+++ src/Pages/Home/components/ShopByBrand.jsx
@@ -2,22 +2,28 @@ import React from 'react'
 import { TitledContainerWithButton } from "../../../Components/TitledContainerWithButton/TitledContainerWithButton";
 import PropTypes from 'prop-types';
 import { BrandCard } from '../../../Components/Cards/BrandCard/BrandCard';
-import { Grid } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 
 
 export default function ShopByBrand({ brands }) {
   return (
-    <TitledContainerWithButton containerTitle="Shop By Brands" buttonTitle={"View all"}>
 
-      <Grid container spacing={{ xs:1, sm: 1, md: 4 }} columns={{ xs: 6, sm: 12 }} sx={{
-        height: { xs: "14rem", sm: "7rem", md: "10.5rem", lg:"13rem" }
+    <TitledContainerWithButton containerTitle="Shop By Brands" buttonTitle={"View all"}>
+      <Box sx={{
+        paddingRight: {
+          xs: ".5rem",
+          sm: "1rem"
+        },
       }}>
+
+      <Grid container spacing={{ xs: 1, sm: 1, md: 4 }} columns={{ xs: 6, sm: 12 }}>
         {brands.map((brand) => (
-          <Grid item xs={2} md={2} key={brand.id} sx={{ paddingRight: "1rem" }} >
+          <Grid item xs={2} md={2} key={brand.id}>
             <BrandCard brand={brand}></BrandCard>
           </Grid>
         ))}
       </Grid>
+      </Box>
 
     </TitledContainerWithButton>
   )
