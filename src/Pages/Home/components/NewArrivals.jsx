93e7862a1acfc2b7c70aa4c7c12fe656cffca166
Mazoda <oabumutlak@gmail.com>
2023-07-22 10:12:54 +0300

    refactor: removed unnecessary style

--- src/Pages/Home/components/NewArrivals.jsx
+++ src/Pages/Home/components/NewArrivals.jsx
@@ -1,37 +1,33 @@
 import React from 'react'
-import {TitledContainerWithButton} from "../../../Components/TitledContainerWithButton/TitledContainerWithButton";
-import {CardsCarousel} from "../../../Components/Cards/CardsCarousel/CardsCarousel";
+import { TitledContainerWithButton } from "../../../Components/TitledContainerWithButton/TitledContainerWithButton";
+import { CardsCarousel } from "../../../Components/Cards/CardsCarousel/CardsCarousel";
 import PropTypes from 'prop-types';
 import { ProductCard } from '../../../Components/Cards/ProductCard/ProductCard';
 import { Box } from '@mui/material';
 
-export default function NewArrivals({products}) {
+export default function NewArrivals({ products }) {
   return (
-    <TitledContainerWithButton  containerTitle="New Arrivals" buttonTitle={"View All"} >
-        <CardsCarousel>
-        {products.map((product)=>(<Box key={product.id} sx={
+    <TitledContainerWithButton containerTitle="New Arrivals" buttonTitle={"View All"} >
+      <Box>
+        
+
+      <CardsCarousel>
+        {products.map((product) => (<Box key={product.id} sx={
           {
             flexShrink: 0,
-            height: {
-                xs: "13rem",
-                sm:"16rem",
-                md: "18rem",
-                lg: "23rem"
-            },
             width: {
-                xs: "8.5rem",
-                sm:"11rem",
-                md: "13rem",
-                lg: "17.75rem"
+              xs: "40%",
+              sm: "33%",
+              md: "20%",
             },
-            marginRight: "1rem"
           }
-        }><ProductCard product={product}  isDetailed={false}></ProductCard></Box>))}
-        </CardsCarousel>
+        }><ProductCard product={product} isDetailed={false}></ProductCard></Box>))}
+      </CardsCarousel>
+      </Box>
     </TitledContainerWithButton>
   )
 }
 
 NewArrivals.propTypes = {
-    products: PropTypes.array
+  products: PropTypes.array
 }
