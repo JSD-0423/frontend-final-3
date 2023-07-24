ef256fa88b977282faa311d4e229fbfa39200a6b
Mazoda <oabumutlak@gmail.com>
2023-07-25 02:16:35 +0300

    Integrated: New Arrivels in Landing page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import HandPicked from "../../Components/HandPicked/HandPicked";
 import NewArrivals from "../../Components/NewArrivals/NewArrivals";
 import ShopByBrand from "../../Components/ShopByBrand/ShopByBrand";
 import HeroSection from "../../Components/HeroSection/HeroSection";
 import Banner from "../../Components/Banner/Banner";
+import { fetchData } from "../../Services/network";
 
 
 //Dummy data
@@ -15,128 +16,6 @@ const collections = [
   { id: 4, title: 'Personal Care', imageSrc: 'https://cdn1.img.sputnikarabic.ae/img/103737/41/1037374157_0:100:1921:1180_600x0_80_0_0_5c139c2a519a6740ea979c29e4aa755e.jpg.webp' }
 ]
 
-const products=[
-  {
-
-    id:'1',
-    name:'Grande',
-    descripton:'Leather Coach Bag with adjustable starps.',
-    brand:'Blossom Pouch',
-    price:{
-      current:'39.49',
-      actual:"78.66",
-      discountPercentage:'50'
-    },
-    rating:"4",
-    totoalRatings:'43',
-
-  imgUrl:'./Assets/laura-chouette-Atomp7YdMaE-unsplash 1.png',
-  fullDescription:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, magnam fuga harum ab aliquid facilis quo accusantium nemo reiciendis rem eaque obcaecati, culpa aliquam illum numquam blanditiis, inventore amet repellendus!"
-   
-  },  {
-
-    id:'2',
-    name:'Grande',
-    descripton:'Leather Coach Bag with adjustable starps.',
-    brand:'Blossom Pouch',
-    price:{
-      current:'39.49',
-      actual:"78.66",
-      discountPercentage:'50'
-    },
-    rating:"4",
-    totoalRatings:'43',
-
-  imgUrl:'./Assets/laura-chouette-Atomp7YdMaE-unsplash 1.png',
-  fullDescription:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, magnam fuga harum ab aliquid facilis quo accusantium nemo reiciendis rem eaque obcaecati, culpa aliquam illum numquam blanditiis, inventore amet repellendus!"
-   
-  },  {
-
-    id:'3',
-    name:'Grande',
-    descripton:'Leather Coach Bag with adjustable starps.',
-    brand:'Blossom Pouch',
-    price:{
-      current:'39.49',
-      actual:"78.66",
-      discountPercentage:'50'
-    },
-    rating:"4",
-    totoalRatings:'43',
-
-  imgUrl:'./Assets/laura-chouette-Atomp7YdMaE-unsplash 1.png',
-  fullDescription:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, magnam fuga harum ab aliquid facilis quo accusantium nemo reiciendis rem eaque obcaecati, culpa aliquam illum numquam blanditiis, inventore amet repellendus!"
-   
-  },  {
-
-    id:'4',
-    name:'Grande',
-    descripton:'Leather Coach Bag with adjustable starps.',
-    brand:'Blossom Pouch',
-    price:{
-      current:'39.49',
-      actual:"78.66",
-      discountPercentage:'50'
-    },
-    rating:"4",
-    totoalRatings:'43',
-
-  imgUrl:'./Assets/laura-chouette-Atomp7YdMaE-unsplash 1.png',
-  fullDescription:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, magnam fuga harum ab aliquid facilis quo accusantium nemo reiciendis rem eaque obcaecati, culpa aliquam illum numquam blanditiis, inventore amet repellendus!"
-   
-  },  {
-
-    id:'5',
-    name:'Grande',
-    descripton:'Leather Coach Bag with adjustable starps.',
-    brand:'Blossom Pouch',
-    price:{
-      current:'39.49',
-      actual:"78.66",
-      discountPercentage:'50'
-    },
-    rating:"4",
-    totoalRatings:'43',
-
-  imgUrl:'./Assets/laura-chouette-Atomp7YdMaE-unsplash 1.png',
-  fullDescription:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, magnam fuga harum ab aliquid facilis quo accusantium nemo reiciendis rem eaque obcaecati, culpa aliquam illum numquam blanditiis, inventore amet repellendus!"
-   
-  },  {
-
-    id:'6',
-    name:'Grande',
-    descripton:'Leather Coach Bag with adjustable starps.',
-    brand:'Blossom Pouch',
-    price:{
-      current:'39.49',
-      actual:"78.66",
-      discountPercentage:'50'
-    },
-    rating:"4",
-    totoalRatings:'43',
-
-  imgUrl:'./Assets/laura-chouette-Atomp7YdMaE-unsplash 1.png',
-  fullDescription:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, magnam fuga harum ab aliquid facilis quo accusantium nemo reiciendis rem eaque obcaecati, culpa aliquam illum numquam blanditiis, inventore amet repellendus!"
-   
-  },  {
-
-    id:'7',
-    name:'Grande',
-    descripton:'Leather Coach Bag with adjustable starps.',
-    brand:'Blossom Pouch',
-    price:{
-      current:'39.49',
-      actual:"78.66",
-      discountPercentage:'50'
-    },
-    rating:"4",
-    totoalRatings:'43',
-
-  imgUrl:'./Assets/laura-chouette-Atomp7YdMaE-unsplash 1.png',
-  fullDescription:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, magnam fuga harum ab aliquid facilis quo accusantium nemo reiciendis rem eaque obcaecati, culpa aliquam illum numquam blanditiis, inventore amet repellendus!"
-   
-  }
-];
 const brands=[
   {
     id:"1",
@@ -164,11 +43,26 @@ const brands=[
     imgUrl:"./Assets/Zara-Logo.png"
   }
 ]
+
 function Home() {
+  const [newArrivals,setNewArrivals] =useState([]);
+  useEffect(()=>{
+    
+      const fetchNewArrivals=async()=>{
+
+      const response=await fetchData('/products/new');
+
+      setNewArrivals(response)
+      }
+
+      fetchNewArrivals(); 
+
+  },[])
+
   return (
     <div>
       <HeroSection/>
-      <NewArrivals products={products}/>
+      <NewArrivals products={newArrivals}/>
       <HandPicked headLine="Handpicked Collections" collections={collections} />
       <ShopByBrand brands={brands} />
       <Banner fullWidthCardText={'Limited edition products'} halfWidthCard1Text={'15% off and more!'} halfWidthCard2Text={'Popular in the community!'}/>
