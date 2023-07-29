feb6ad1733ab3a6a82687688885d6a2d88750ed5
Mazoda <oabumutlak@gmail.com>
2023-07-29 14:53:29 +0300

    Handeled Loading, Error and Not found

--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -4,20 +4,29 @@ import { useParams } from "react-router-dom";
 import { Box, Grid, Typography } from '@mui/material';
 import BreadCrumb from '../../Components/BreadCrumb/BreadCrumb';
 import { fetchData } from "../../Services/network";
-import LoadingButton from '@mui/lab/LoadingButton';
+
+
 
 
 function Category() {
     const { targetAPI } = useParams();
     const [products, setProducts] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+
+
 
     useEffect(() => {
         const fetchDataAsync = async () => {
+            setLoading(true);
             try {
                 const result = await fetchData(`/products/${targetAPI}`);
                 setProducts(result);
+                setLoading(false);
+
             } catch (error) {
-                console.error(error);
+                setLoading(false)
+                setError(error.message);
             }
         }
         fetchDataAsync()
@@ -41,28 +50,71 @@ function Category() {
             }}>
                 {targetAPI}
             </Typography>
-            <Box sx={{ flexGrow: 1, paddingInline: "1rem", marginBlock: "2rem" }} >
-                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 6, md: 12, lg: 15 }}>
-                    {products ? products.map((product) => (
-                        <Grid item xs={2} sm={2} md={3} lg={3} key={product.id}>
-                            <ProductCard product={product} isDetailed={true} />
-                        </Grid>
-                    )) :
-                        <Box sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center", height: "50vh",
-                            width: "100%"
-                        }}>
-                            <LoadingButton
-                                size="large"
-                                loadingIndicator="Loading…"
-                                loading={true}
-                                disabled
-                            />
-                        </Box>}
-                </Grid>
-            </Box>
+
+            {error ? (
+                <Box sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center", height: "50vh",
+                    width: "100%"
+                }}>
+                    <Typography
+                        sx={{
+                            color: "primary.main",
+                            fontSize: {
+                                xs: '1rem',
+                                sm: "2rem"
+                            },
+                            fontWeight: "fontWeightLabelSmall"
+                        }}
+                    >
+                        {error}
+                    </Typography></Box>
+            ) : loading ? (
+                <Box sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center", height: "50vh",
+                    width: "100%"
+                }}>
+                    <Typography
+                        sx={{
+                            color: "primary.main",
+                            fontSize: {
+                                xs: '1rem',
+                                sm: "2rem"
+                            },
+                            fontWeight: "fontWeightLabelSmall"
+                        }}
+                    >
+                        Loading...
+                    </Typography>
+                </Box>
+            ) : products.length === 0 ? <Typography
+                sx={{
+                    color: "primary.main",
+                    fontSize: {
+                        xs: '1rem',
+                        sm: "2rem"
+                    },
+                    fontWeight: "fontWeightLabelSmall"
+                }}
+            >
+                No Products Found
+            </Typography> : (
+
+                <Box sx={{ flexGrow: 1, paddingInline: "1rem", marginBlock: "2rem" }} >
+                    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 6, md: 12, lg: 15 }}>
+                        {products.map((product) => (
+                            <Grid item xs={2} sm={2} md={3} lg={3} key={product.id}>
+                                <ProductCard product={product} isDetailed={true} />
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Box>
+            )}
+
+
         </>
     )
 }
