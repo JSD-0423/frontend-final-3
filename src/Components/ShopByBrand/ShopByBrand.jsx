3ac9cf3bb901ae066c1442ff5be80c2770469304
halasalhab <halasalhab0@gmail.com>
2023-07-31 10:31:56 +0200

    added error and loading handling in shope by brand

--- src/Components/ShopByBrand/ShopByBrand.jsx
+++ src/Components/ShopByBrand/ShopByBrand.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { TitledContainerWithButton } from "../TitledContainerWithButton/TitledContainerWithButton";
-import PropTypes from 'prop-types';
 import { BrandCard } from '../Cards/BrandCard/BrandCard';
-import { Box, Grid } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { fetchData } from '../../Services/network';
+
+export default function ShopByBrand() {
+
+  const [brands, setBrands] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
 
 
-export default function ShopByBrand({ brands, linkToValue }) {
+  useEffect(() => {
+    const fetchDataAsync = async () => {
+      setLoading(true);
+      try {
+        const result = await fetchData(`https://store-osn9.onrender.com/brands`);
+        setBrands(result);
+      } catch (error) {
+        setError(error.message);
+      }
+      setLoading(false);
+    }
+    fetchDataAsync();
+  }, [])
   return (
 
     <TitledContainerWithButton containerTitle="Shop By Brands" buttonTitle={"View all"}>
@@ -16,23 +34,76 @@ export default function ShopByBrand({ brands, linkToValue }) {
           sm: "1rem"
         },
       }}>
-
-      <Grid container spacing={{ xs: 1, sm: 1, md: 4 }} columns={{ xs: 6, sm: 12 }}>
-        {brands ? brands.map((brand) => (
-          <Grid item xs={2} md={2} key={brand.id}>
-            <Link to={linkToValue+brand.id} style={{textDecoration: 'none'}}>
-            <BrandCard brand={brand}></BrandCard>
-            </Link>
-          </Grid>
-        )) : 'no data found'}
-      </Grid>
+      {error ? (
+                <Box sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center", height: "50vh",
+                    width: "100%"
+                }}>
+                    <Typography
+                        sx={{
+                            color: "primary.main",
+                            fontSize: {
+                                xs: '1rem',
+                                sm: "2rem"
+                            },
+                            fontWeight: "fontWeightLabelSmall"
+                        }}
+                    >
+                        {error}
+                    </Typography></Box>
+            ): loading ? (
+              <Box sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center", height: "50vh",
+                  width: "100%"
+              }}>
+                  <Typography
+                      sx={{
+                          color: "primary.main",
+                          fontSize: {
+                              xs: '1rem',
+                              sm: "2rem"
+                          },
+                          fontWeight: "fontWeightLabelSmall"
+                      }}
+                  >
+                      Loading...
+                  </Typography>
+              </Box>
+          ) : brands.length === 0 ? 
+          <Box sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center", height: "20vh",
+            width: "100%"
+        }}>
+            <Typography
+                sx={{
+                    color: "primary.main",
+                    fontSize: {
+                        xs: '1rem',
+                        sm: "2rem"
+                    },
+                    fontWeight: "fontWeightLabelSmall"
+                }}
+            >
+                No Products Found
+            </Typography>
+        </Box> : 
+        (<Grid container spacing={{ xs: 1, sm: 1, md: 4 }} columns={{ xs: 6, sm: 12 }}>
+          {brands ? brands.map((brand) => (
+            <Grid item xs={2} md={2} key={brand.id}>
+              <Link to={`products/brands/+${brand.id}`} style={{ textDecoration: 'none' }}>
+                <BrandCard brand={brand}></BrandCard>
+              </Link>
+            </Grid>
+          )) : 'no data found'}
+        </Grid>)}
       </Box>
 
     </TitledContainerWithButton>
   )
-}
-
-ShopByBrand.propTypes = {
-  brands: PropTypes.array,
-  linkToValue: PropTypes.string
 }
