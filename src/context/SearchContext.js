2f5fe5196cbc264665597939e161cecdd30410d3
Mazoda <oabumutlak@gmail.com>
2023-08-02 20:41:10 +0300

    feat: Search Context

---
+++ src/context/SearchContext.js
@@ -0,0 +1,23 @@
+// SearchContext.js
+import React, { createContext, useState} from "react";
+import PropTypes from 'prop-types';
+
+const SearchContext = createContext();
+
+const SearchProvider = ({ children }) => {
+  const [keyword, setKeyword] = useState("");
+
+  return (
+    <SearchContext.Provider value={{ keyword, setKeyword }}>
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+
+
+SearchProvider.propTypes = {
+    children: PropTypes.any
+  
+  }
+export { SearchProvider,SearchContext };
