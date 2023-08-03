782e8a412e5b60bc83def75f34012d529618ace2
Mazoda <oabumutlak@gmail.com>
2023-08-03 06:25:47 +0300

    feat: activated navigation in BreadCrumb

--- src/Components/BreadCrumb/BreadCrumb.jsx
+++ src/Components/BreadCrumb/BreadCrumb.jsx
@@ -2,33 +2,36 @@ import { Box, Typography } from '@mui/material'
 import React from 'react'
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-export default function BreadCrumb({ MainTitle, SubTitle, ProductTitle }) {
+export default function BreadCrumb({ MainTitle, SubTitle, ProductTitle, SubTitleRoute }) {
     return (
         <Box sx={{
             display: "flex",
             gap: "1rem",
             alignItems: "center",
             justifyContent: "flex-start",
-            paddingInline:"1rem",
-            marginBlock:"1rem"
+            paddingInline: "1rem",
+            marginBlock: "1rem"
         }}>
 
-            <Typography variant='h1' sx={{
-                fontSize: "1rem",
-                color: "primary.main",
-                fontWeight: "fontWeightMedium"
-            }}>
-                {MainTitle}
+            <Link to={'/'}>
+                <Typography variant='h1' sx={{
+                    fontSize: "1rem",
+                    color: "primary.main",
+                    fontWeight: "fontWeightMedium"
+                }}>
+                    {MainTitle}
 
-            </Typography>
+                </Typography>
+            </Link>
             <NavigateNextIcon sx={{
                 fontSize: "1.5rem"
             }} />
 
             {ProductTitle ?
                 <>
-                    <Typography
+                    <Link to={SubTitleRoute}><Typography
                         sx={{
                             fontSize: "1rem",
                             color: "primary.main",
@@ -36,7 +39,7 @@ export default function BreadCrumb({ MainTitle, SubTitle, ProductTitle }) {
                             fontWeight: "fontWeightMedium"
 
                         }}
-                    >{SubTitle}</Typography>
+                    >{SubTitle}</Typography></Link>
                     <NavigateNextIcon sx={{
                         fontSize: "1.5rem"
                     }} />
@@ -68,5 +71,6 @@ export default function BreadCrumb({ MainTitle, SubTitle, ProductTitle }) {
 BreadCrumb.propTypes = {
     MainTitle: PropTypes.string,
     SubTitle: PropTypes.string,
+    SubTitleRoute:PropTypes.string,
     ProductTitle: PropTypes.string
 }
