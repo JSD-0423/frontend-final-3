6602a2e921c0bd539fed67b0d55f4608fc25cbe7
Mazoda <oabumutlak@gmail.com>
2023-08-03 10:37:52 +0300

    Add: Mobile search input

--- src/Pages/PageNotFound/PageNotFound.jsx
+++ src/Pages/PageNotFound/PageNotFound.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import { Box, Typography,Link } from '@mui/material'
+import { Box, Typography, Link } from '@mui/material'
 import "./style.css";
+import { MobileSearchInput } from "../../Components/InputWithIcon/MobileSearchInput";
 
 function PageNotFound() {
-  
+
   return (
-    <Box width="100%" sx={{display: "flex", flexDirection: "column", alignItems: "center", py: "5rem"}}>
-      <Typography variant="h2" color="primary" sx={{pb: "2rem", fontWeight: "bold"}}>
+    <Box width="100%" sx={{ display: "flex", flexDirection: "column", alignItems: "center", py: "5rem" }}>
+      <MobileSearchInput />
+      <Typography variant="h2" color="primary" sx={{ pb: "2rem", fontWeight: "bold" }}>
         Oops!
       </Typography>
-      <Typography variant="h3" color="primary" sx={{pb: "2rem", fontWeight: "bold"}}>
+      <Typography variant="h3" color="primary" sx={{ pb: "2rem", fontWeight: "bold" }}>
         Page Not Found!
       </Typography>
       <Link className="link" href="/" underline="none">
-        <Typography variant="body1" color="ActiveBorder" sx={{pb: "2rem", fontWeight: "bold"}}>
+        <Typography variant="body1" color="ActiveBorder" sx={{ pb: "2rem", fontWeight: "bold" }}>
           Go Home Page
         </Typography>
       </Link>
